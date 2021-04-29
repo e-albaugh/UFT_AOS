1e664ea7d1db028900ff026f28973ce4741e49a5
mason <mason.henry@microfocus.com>
2021-04-29 18:06:42 +0000

    commit

---
+++ AOS_Home/Action1/Script.mts
@@ -0,0 +1,1 @@
+﻿Browser("Advantage Shopping").Page("Advantage Shopping").Link("dvantage DEMO").Click @@ script infofile_;_ZIP::ssf1.xml_;_
---
+++ AOS_Home/Res1/Report/AOS_Home_3/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\demo\\Documents\\Unified Functional Testing\\AOS_Home\\Res1\\Report\\AOS_Home_3',namePairs:[]}]};
